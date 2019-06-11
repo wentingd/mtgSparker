afda9534af6d6b06983c87467646d307f2f761c7
wenting-duan <wenting.duan@axa.co.jp>
2019-06-11 18:08:56 +0900

    use fa icon to show dice as pips instead of number

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {
 } from 'react-native';
 import {
   faFire, faDice, faCogs, faRedo, faUsers, faUserFriends,
+  faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix,
 } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import LifeCounterView from './components/LifeCounterView';
@@ -12,6 +13,7 @@ import { generatePlayers } from './components/Utils';
 
 library.add(
   faFire, faDice, faCogs, faRedo, faUsers, faUserFriends,
+  faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix,
 );
 
 export default class App extends React.Component {
--- components/Dice.js
+++ components/Dice.js
@@ -1,49 +1,53 @@
 import React from 'react';
 import {
-  View, Text, StyleSheet,
+  View, StyleSheet,
 } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 const styles = StyleSheet.create({
-  face: {
-    // justifyContent: 'center',
-    // alignItem: 'center',
-    height: 80,
-    width: 80,
-    borderRadius: 15,
+  container: {
+    justifyContent: 'center',
+    borderRadius: 20,
     borderWidth: 3,
     borderColor: 'gainsboro',
     display: 'flex',
-    justifyContent: 'space-around',
-  },
-  faceText: {
-    textAlign: 'center',
-    color: 'white',
-    fontSize: 50,
   },
   pip: {
-    height: 20,
-    width: 20,
-    borderRadius: 20,
-    backgroundColor: 'gainsboro',
+    color: 'gainsboro',
   },
+  // faceText: {
+  //   textAlign: 'center',
+  //   color: 'white',
+  //   fontSize: 50,
+  // },
 });
+
+const convertFaceCountToWord = (number) => {
+  const map = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+  };
+  return map[number];
+};
 
 class Dice extends React.Component {
   render() {
-    const { faceCount } = this.props;
+    const { faceCount, color } = this.props;
     return (
-      <View style={{ ...styles.face, backgroundColor: this.props.color || 'rgba(0, 0, 0, 0.7)' }}>
-        {/* {
-          [1, 2, 3, 4, 5].map((index) => (
-            <View
-              key={index}
-              style={styles.pip}
-            />
-          ))
-        } */}
-        <Text style={styles.faceText}>
-          {faceCount}
-        </Text>
+      <View style={{
+        ...styles.container,
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      }}
+      >
+        <FontAwesomeIcon
+          icon={`dice-${convertFaceCountToWord(faceCount)}`}
+          size={75}
+          style={styles.pip}
+        />
       </View>
     );
   }
